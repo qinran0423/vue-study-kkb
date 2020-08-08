340703bd253cf0e9bed8e6e2f35818af110a61cf
randy0423 <1156107187@qq.com>
2020-08-08 09:03:30 +0800

    kk

--- vue-components/src/router/index.js
+++ vue-components/src/router/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import VueRouter from './rrouter'
+import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -21,6 +21,7 @@ Vue.use(VueRouter)
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
