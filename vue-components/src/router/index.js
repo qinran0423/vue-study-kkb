d40f784cc6ed20bd17a148950227445c0fbe93c7
randy0423 <1156107187@qq.com>
2020-08-20 07:47:42 +0800

    权限控制待完成

--- vue-components/src/router/index.js
+++ vue-components/src/router/index.js
@@ -1,36 +1,36 @@
 import Vue from 'vue'
-import VueRouter from './rrouter'
-import Home from '../views/Home.vue'
+import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
+// 通用页面：  不需要首尾，可直接访问
+export const constRoutes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    component: () => import('@/views/Login.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-    children: [
-      {
-        path: '/about/info',
-        name: 'Info',
-        component: () => import(/* webpackChunkName: "about" */ '../views/Info.vue'),
-      }
-    ]
+    path: '/',
+    component: () => import('@/views/Home.vue')
   }
 ]
 
+
+export const asyncRoutes = [
+  {
+    path: '/about',
+    component: () => import('@/views/About.vue'),
+    meta: {
+      roles: ['admin']
+    }
+  }
+]
+
+
 const router = new VueRouter({
-  mode: 'hash',
+  mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: constRoutes
 })
 
 export default router
---
+++ vue-components/src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>用户登录</h2>
+    <div>
+      <input type="text" v-model="username" />
+      <button @click="login">登录</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "admin",
+    };
+  },
+  methods: {
+    login() {},
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
